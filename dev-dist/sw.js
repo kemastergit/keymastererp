07f37d9310d9ccb83dda9ccee7c5c9f72bce8d30
Tu Nombrejuan <juanluis@example.com>
2026-05-22 15:56:19 -0400

    checkpoint: pre editable price in compras

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ep6ericsmu"
+    "revision": "0.k9ue6vhenro"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
